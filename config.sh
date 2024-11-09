ff8b112bcf43b31ecee5828115b693e0e5b828b8
Dmitry Sukhodoev <raven428@gmail.com>
2024-11-09 13:44:46 +0500

    add config script

---
+++ config.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -ueo pipefail
+: "${XUI:="3x-ui"}"
+: "${XUIV:="${XUI}-2_4_5-arm64-static-musl"}"
+: "${XURI:="https://github.com/raven428/container-images/releases/download/000/${XUIV}.tar.xz"}"
+if [[ -n "${WRT_SET_PASSWD:-}" ]]; then
+  /usr/bin/env printf "${WRT_SET_PASSWD}\n${WRT_SET_PASSWD}\n" |
+    /usr/bin/env passwd
+else
+  echo "skipped passwd, [export WRT_SET_PASSWD=pwd4dev] to set "
+fi
+(
+  /usr/bin/env mkdir -vp /opt/3x-ui/bin
+  cd /opt/3x-ui
+  /usr/bin/env curl -Lm 11 -o - "${XURI}" |
+    /bin/tar xJvf -
+  /usr/bin/env ln -sfv "${XUIV}" "${XUI}"
+)
