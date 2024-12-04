a209ff7b30f9fed3bfed51c1d46544fa519e3c35
Dmitry Sukhodoev <raven428@gmail.com>
2024-12-04 12:51:12 +0500

    add warp to dokodemo

--- files/etc/hotplug.d/iface/33-acme.sh
+++ files/etc/hotplug.d/iface/33-acme.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-[[ "${ACTION}" == "ifup" && "${INTERFACE}" == "wan" ]] || exit 0
+[[ "${ACTION}" == "ifup" && "${DEVICE}" == "br-wan" ]] || exit 0
 /etc/init.d/acme start 2>&1 |
   /usr/bin/env logger -t "acme-startup"
