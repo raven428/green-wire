d862520b77e9268308ac29432e0d8924086dc095
Dmitry Sukhodoev <raven428@gmail.com>
2024-12-24 12:20:58 +0500

    exampleof ipv6 stack enable for device itself

--- files/etc/hotplug.d/iface/33-acme.sh
+++ files/etc/hotplug.d/iface/33-acme.sh
@@ -2,3 +2,5 @@
 [[ "${ACTION}" == "ifup" && "${DEVICE}" == "br-wan" ]] || exit 0
 /etc/init.d/acme start 2>&1 |
   /usr/bin/env logger -t "acme-startup"
+# /usr/bin/env ip route add ::/0 dev br-wan 2>&1 |
+#   /usr/bin/env logger -t "route-wan"
