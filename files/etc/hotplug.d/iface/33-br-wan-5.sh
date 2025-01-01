f51daf26f3f92704ffb6f5bcbb67c812c9246ebd
Dmitry Sukhodoev <raven428@gmail.com>
2025-01-01 10:56:33 +0500

    set networks explicit vlans

--- files/etc/hotplug.d/iface/33-br-wan-5.sh
+++ files/etc/hotplug.d/iface/33-br-wan-5.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-[[ "${ACTION}" == "ifup" && "${DEVICE}" == "br-wan" ]] || exit 0
+[[ "${ACTION}" == "ifup" && "${DEVICE}" == "br-wan.5" ]] || exit 0
 /etc/init.d/acme start 2>&1 |
   /usr/bin/env logger -t "acme-startup"
 # /usr/bin/env ip route add ::/0 dev br-wan 2>&1 |
