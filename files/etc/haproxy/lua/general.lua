27708c7080b4e9d406961dde2a309b5721431aa9
Dmitry Sukhodoev <raven428@gmail.com>
2025-01-09 17:58:05 +0500

    add haproxy configuration

---
+++ files/etc/haproxy/lua/general.lua
@@ -0,0 +1,24 @@
+-- https://habr.com/ru/articles/531004/
+-- https://www.arpalert.org/src/haproxy-lua-api/2.3/index.html
+core.register_action(
+  "get_iso8601_time", { "http-req", "tcp-req", "http-res" }, function(txn)
+    txn:set_var(
+      "txn.iso8601_time",
+      string.format(
+        "%s.%03d",
+        os.date("!%Y-%m-%dT%H:%M:%S"),
+        math.floor(core.now().usec / 1000)
+      )
+    )
+  end
+)
+
+core.register_fetches(
+  "iso8601-now", function(txn)
+   return string.format(
+      "%s.%03d",
+      os.date("!%Y-%m-%dT%H:%M:%S"),
+      math.floor(core.now().usec / 1000)
+    )
+  end
+);
