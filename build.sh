7d3cc261c3b22a6505c58abdb654d8e931177896
Dmitry Sukhodoev <raven428@gmail.com>
2025-03-25 18:32:19 +0500

    add WRT_WARP_REGIONS and WRT_OTHER_ROUTES options

--- build.sh
+++ build.sh
@@ -81,7 +81,10 @@ export WRT_L2TP_PASSWD='${WRT_L2TP_PASSWD:-"l2tp-password"}'
 export WRT_LAN2WAN_TAG='${WRT_LAN2WAN_TAG:-"null"}'
 export WRT_ACME_STAR='${WRT_ACME_STAR:-"null"}'
 export WRT_IPSEC_PSK='${WRT_IPSEC_PSK:-"s3cre1P5k4ipSek4pr0tect"}'
+export WRT_WARP_REGIONS='${WRT_WARP_REGIONS:-"m"}'
 export WRT_WARP_REG='${WRT_WARP_REG:-"172.16.0.2,26:06:47::00,private_key"}'
+export WRT_OTHER_ROUTES='${WRT_OTHER_ROUTES:-"tr cy kz ge de us ng cl bg it ar uy \
+se nl cz"}'
 export WRT_CLIENTS='${WRT_CLIENTS:-"caga@50:e5:49:cb:b5:67#1,
 dir300@00:21:91:31:98:60#99"}'
 EOF
