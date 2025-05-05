9ddfacdb6b0ec709b0b4b6f138ec789d532bcc39
Dmitry Sukhodoev <raven428@gmail.com>
2025-05-05 18:25:31 +0500

    add devices configs

---
+++ configs/garden.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+export WRT_HOSTNAME='garden'
+export WRT_WIFI_SSID="${WRT_HOSTNAME}"
+export WRT_ARCH='aarch64_cortex-a53'
+export WRT_BOARD='sunxi/cortexa53'
+export WRT_KMODS='6.6.86-1-d0617ab163ece80de863452c22026024'
+export WRT_BUILDER='ghcr.io/raven428/container-images/owrt-sxi-cora53-24_10_1:000'
+export WRT_PROFILE='xunlong_orangepi-zero3'
+export WRT_WIFI_MAC_FILTER='allow'
+export WRT_LAN2WAN_TAG='null'
+# export WRT_ACME_STAR='null' – use default null
+export WRT_SIZE_ROOT='2222'
+export WRT_PORT_FORWARD='null'
+export WRT_ADD_PKGS='kmod-rtw88-8821cu kmod-carl9170 wpad-basic-mbedtls'
+export WRT_SKIP_INSTALLER='yes'
+export WRT_SIZE_EXTRA='12222'
+export WRT_CO_SUFFIX='op3z'
---
+++ configs/hearts.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+export WRT_HOSTNAME='hearts'
+export WRT_WIFI_SSID="${WRT_HOSTNAME}"
+export WRT_ARCH='aarch64_cortex-a53'
+export WRT_BOARD='mediatek/filogic'
+export WRT_KMODS='6.6.86-1-6ace983a14b769f576fe9c4c7961bd89'
+export WRT_BUILDER='ghcr.io/raven428/container-images/owrt-mtk-filogic-24_10_1:000'
+export WRT_PROFILE='bananapi_bpi-r3'
+# export WRT_WIFI_MAC_FILTER='allow' – use default null
+export WRT_LAN2WAN_TAG='null'
+# export WRT_ACME_STAR='null' – use default null
+export WRT_SIZE_ROOT='2222'
+export WRT_PORT_FORWARD='null'
+export WRT_ADD_PKGS='nand-utils kmod-nvme'
